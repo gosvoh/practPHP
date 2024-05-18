335645d5d391509fcc33fb67197450352448331a
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-05-19 01:32:37 +0300

    Редактирование скриптов для первой лабораторной

--- data/init.php
+++ data/init.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 // блок инициализации
 try {
 	$pdoSet = new PDO('mysql:host=localhost', 'root', '');
 	$pdoSet->query('SET NAMES utf8;');
 } catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
 }
 
 // код для "неубиваемой" базы данных
@@ -14,26 +14,46 @@ $stmt = $pdoSet->query($sqlTM);
 $sqlTM = "USE test;";
 $stmt = $pdoSet->query($sqlTM);
 
-$sqlTM = "CREATE TABLE IF NOT EXISTS myArtTable (id int(11) NOT NULL auto_increment, text text NOT NULL, description text NOT NULL, keywords text NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=cp1251;";
-$stmt = $pdoSet->query($sqlTM);
-$sqlTM = "CREATE TABLE IF NOT EXISTS files (id_file int(11) NOT NULL auto_increment, id_my int(11) NOT NULL, description text NOT NULL, name_origin text NOT NULL, path text NOT NULL, date_upload text NOT NULL, PRIMARY KEY (id_file), FOREIGN KEY (id_my) REFERENCES myarttable(id)) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=cp1251;";
+$sqlTM = "
+CREATE TABLE IF NOT EXISTS `Individuals` (
+	`id` int NOT NULL,
+	`first_name` varchar(30) NOT NULL,
+	`last_name` varchar(30) NOT NULL,
+	`middle_name` varchar(30) DEFAULT NULL,
+	`passport` varchar(30) NOT NULL,
+	`INN` varchar(12) NOT NULL,
+	`SNILS` varchar(11) NOT NULL,
+	`license` varchar(30) DEFAULT NULL,
+	`additional_docs` varchar(30) DEFAULT NULL,
+	`notes` varchar(255) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+  );
+";
 $stmt = $pdoSet->query($sqlTM);
 // конец кода для "неубиваемой" базы данных
 
 if (isset($_GET['bt1'])) {
 	// работает независимо от кол-ва столбцов.
-	$sql = "SHOW COLUMNS FROM myarttable";
+	$sql = "SHOW COLUMNS FROM Individuals";
 	$stmt = $pdoSet->query($sql);
 	$resultMF = $stmt->fetchAll();
-	$sqlTM = "INSERT INTO myarttable (";
-	for($iR=1; $iR < Count($resultMF); ++$iR) {
+	$sqlTM = "INSERT INTO Individuals (";
+	for ($iR = 1; $iR < Count($resultMF); ++$iR) {
 		$sqlTM .= $resultMF[$iR]["Field"];
-		if ($iR < Count($resultMF)-1) { $sqlTM .= ', '; } else { $sqlTM .= ") VALUES ("; }
+		if ($iR < Count($resultMF) - 1) {
+			$sqlTM .= ', ';
+		} else {
+			$sqlTM .= ") VALUES (";
+		}
 	}
-	
-	for($iR=1; $iR < Count($resultMF); ++$iR) {
-		$sqlTM .= "'".$_GET[$resultMF[$iR]["Field"]]."'";
-		if ($iR < Count($resultMF)-1) { $sqlTM .= ', '; } else { $sqlTM .= ")"; }
+
+	for ($iR = 1; $iR < Count($resultMF); ++$iR) {
+		$sqlTM .= "'" . $_GET[$resultMF[$iR]["Field"]] . "'";
+		if ($iR < Count($resultMF) - 1) {
+			$sqlTM .= ', ';
+		} else {
+			$sqlTM .= ")";
+		}
 	}
 
 	$stmt = $pdoSet->query($sqlTM);
@@ -42,46 +62,47 @@ if (isset($_GET['bt1'])) {
 // начало вставки для UPDATE
 if (isset($_GET['textId'])) {
 	// работает независимо от кол-ва столбцов.
-	$sql = "SHOW COLUMNS FROM myarttable";
+	$sql = "SHOW COLUMNS FROM Individuals";
 	$stmt = $pdoSet->query($sql);
 	$resultMF = $stmt->fetchAll();
-	$sqlTM = "UPDATE myarttable SET ";
-	for($iR=1; isset($_GET["textEd".$iR]); ++$iR) {
-		$sqlTM .= $resultMF[$iR]["Field"]."='".$_GET["textEd".$iR]."'";
-		if (isset($_GET["textEd".($iR+1)])) { $sqlTM .= ', '; } else { $sqlTM .= " WHERE id = " . $_GET["textId"]; }
+	$sqlTM = "UPDATE Individuals SET ";
+	for ($iR = 1; isset($_GET["textEd" . $iR]); ++$iR) {
+		$sqlTM .= $resultMF[$iR]["Field"] . "='" . $_GET["textEd" . $iR] . "'";
+		if (isset($_GET["textEd" . ($iR + 1)])) {
+			$sqlTM .= ', ';
+		} else {
+			$sqlTM .= " WHERE id = " . $_GET["textId"];
+		}
 	}
-	
-	$stmt = $pdoSet->query($sqlTM);	
+
+	$stmt = $pdoSet->query($sqlTM);
 }
 // конец вставки для UPDATE
 
 
 // начало вставки для DELETE
 if (isset($_GET['delid'])) {
-	$sqlTM = "DELETE FROM files WHERE id_my = " . $_GET["delid"];
-	$stmt = $pdoSet->query($sqlTM);
-	$sqlTM = "DELETE FROM myarttable WHERE id = " . $_GET["delid"];
+	$sqlTM = "DELETE FROM Individuals WHERE id = " . $_GET["delid"];
 	$stmt = $pdoSet->query($sqlTM);
 }
 // конец вставки для DELETE
 
 // добавление столбца.
 if (isset($_GET['addrow'])) {
-	$sqlTM = "ALTER TABLE myarttable ADD ".$_GET['addrow']."1 TEXT NOT NULL AFTER ".$_GET['addrow'];
+	$sqlTM = "ALTER TABLE Individuals ADD " . $_GET['addrow'] . "1 TEXT NOT NULL AFTER " . $_GET['addrow'];
 	$stmt = $pdoSet->query($sqlTM);
 }
 // удаление столбца.
 if (isset($_GET['delrow'])) {
-	$sqlTM = "ALTER TABLE myarttable DROP ".$_GET['delrow'];
+	$sqlTM = "ALTER TABLE Individuals DROP " . $_GET['delrow'];
 	$stmt = $pdoSet->query($sqlTM);
 }
-	
+
 // основной запрос для выгрузки массива данных из таблицы.
 if (isset($_GET['order'])) {
-	$sql = "SELECT * FROM myarttable WHERE id>14 ORDER BY ".$_GET['order']." DESC";
+	$sql = "SELECT * FROM Individuals ORDER BY " . $_GET['order'] . " DESC";
 } else {
-	$sql = "SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC";  // ASC - по возрастанию; DESC - по убыванию.
+	$sql = "SELECT * FROM Individuals ORDER BY id DESC";  // ASC - по возрастанию; DESC - по убыванию.
 }
-	$stmt = $pdoSet->query($sql);
-	$resultMF = $stmt->fetchAll(PDO::FETCH_NUM); // PDO::FETCH_NUM - только числовые индексы: [0][0]
-?>
+$stmt = $pdoSet->query($sql);
+$resultMF = $stmt->fetchAll(PDO::FETCH_NUM); // PDO::FETCH_NUM - только числовые индексы: [0][0]
--- test.sql
+++ test.sql
@@ -3,64 +3,30 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS test;
+DROP DATABASE IF EXISTS `test`;
+CREATE DATABASE test;
 
-CREATE TABLE `files` (
-  `id_file` int(11) NOT NULL,
-  `id_my` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `name_origin` text NOT NULL,
-  `path` text NOT NULL,
-  `date_upload` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
+USE test;
 
+CREATE TABLE `Individuals` (
+  `id` int NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `middle_name` varchar(30) DEFAULT NULL,
+  `passport` varchar(30) NOT NULL,
+  `INN` varchar(12) NOT NULL,
+  `SNILS` varchar(11) NOT NULL,
+  `license` varchar(30) DEFAULT NULL,
+  `additional_docs` varchar(30) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
-INSERT INTO files (`id_file`, `id_my`, `description`, `name_origin`, `path`, `date_upload`) VALUES
-(48, 25, 'Закачка из менеджера', '3-Март.jpg', 'files/3-Март.jpg', '16-02-2022  02:00:11'),
-(49, 23, 'Закачка из менеджера', '1-Январь.jpg', 'files/1-Январь.jpg', '17-02-2022  08:20:24'),
-(50, 23, 'Закачка из менеджера', '2-Февраль.jpg', 'files/2-Февраль.jpg', '17-02-2022  08:20:24'),
-(51, 23, 'Закачка из менеджера', '3-Март.jpg', 'files/3-Март.jpg', '17-02-2022  08:20:24'),
-(52, 23, 'Закачка из менеджера', '4-Апрель.jpg', 'files/4-Апрель.jpg', '17-02-2022  08:20:37'),
-(53, 23, 'Закачка из менеджера', '5-Май.jpg', 'files/5-Май.jpg', '17-02-2022  08:20:37'),
-(54, 23, 'Закачка из менеджера', '6-Июнь.jpg', 'files/6-Июнь.jpg', '17-02-2022  08:20:37'),
-(55, 23, 'Закачка из менеджера', '7-Июль.jpg', 'files/7-Июль.jpg', '17-02-2022  08:20:53'),
-(56, 23, 'Закачка из менеджера', '8-Август.jpg', 'files/8-Август.jpg', '17-02-2022  08:20:53'),
-(57, 23, 'Закачка из менеджера', '9-Сентябрь.jpg', 'files/9-Сентябрь.jpg', '17-02-2022  08:20:53'),
-(58, 23, 'Закачка из менеджера', '10-Октябрь.jpg', 'files/10-Октябрь.jpg', '17-02-2022  08:21:11'),
-(59, 23, 'Закачка из менеджера', '11-Ноябрь.jpg', 'files/11-Ноябрь.jpg', '17-02-2022  08:21:11'),
-(60, 23, 'Закачка из менеджера', '12-Декабрь.jpg', 'files/12-Декабрь.jpg', '17-02-2022  08:21:11'),
-(61, 23, 'Закачка из менеджера', 'lecture_1.pdf', 'files/lecture_1.pdf', '17-02-2022  08:21:33'),
-(62, 23, 'Закачка из менеджера', 'lecture_2.pdf', 'files/lecture_2.pdf', '17-02-2022  08:21:33'),
-(63, 23, 'Закачка из менеджера', 'test.sql', 'files/test.sql', '17-02-2022  08:21:33');
+INSERT INTO `Individuals` (`id`, `first_name`, `last_name`, `middle_name`, `passport`, `INN`, `SNILS`, `license`, `additional_docs`, `notes`) VALUES
+(1, 'Иван', 'Иванов', 'Иванович', '1234 567890', 123456789012, 12345678901, '1234 567890', '1234 567890', '1234 567890'),
+(2, 'Петр', 'Петров', 'Петрович', '1234 567890', 123456789012, 12345678901, '1234 567890', '1234 567890', '1234 567890'),
+(3, 'Сидор', 'Сидоров', 'Сидорович', '1234 567890', 123456789012, 12345678901, '1234 567890', '1234 567890', '1234 567890'),
+(4, 'Александр', 'Александров', 'Александрович', '1234 567890', 123456789012, 12345678901, '1234 567890', '1234 567890', '1234 567890'),
+(5, 'Андрей', 'Андреев', 'Андреевич', '1234 567890', 123456789012, 12345678901, '1234 567890', '1234 567890', '1234 567890');
 
-CREATE TABLE `myarttable` (
-  `id` int(11) NOT NULL,
-  `text` text NOT NULL,
-  `description` text NOT NULL,
-  `keywords` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
-
-INSERT INTO `myarttable` (`id`, `text`, `description`, `keywords`) VALUES
-(20, 'Февраль', '128', 'Сидоров В.Н.'),
-(21, 'Март', '520', 'Васильев Н.К.'),
-(23, 'Май', '629', 'Смирнов В.Ю.'),
-(24, 'Июнь', '371', 'Петров К.Д.'),
-(25, 'Июль', '542', 'Субботин А.Н.'),
-(26, 'Август', '389', 'Данилин В.Д.');
-
-ALTER TABLE `files`
-  ADD PRIMARY KEY (`id_file`),
-  ADD KEY `id_my` (`id_my`);
-
-ALTER TABLE `myarttable`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `files`
-  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
-
-ALTER TABLE `myarttable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-
-ALTER TABLE `files`
-  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`id_my`) REFERENCES `myarttable` (`id`);
 COMMIT;
